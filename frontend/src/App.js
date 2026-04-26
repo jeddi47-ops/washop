7cbf2180be84c7e863c8cbe42c6ad08b3cac80e2
jeddi47-ops <jedidiadinga47@gmail.com>
2026-04-26 14:49:31 +0100

    Add ResetPassword route and component import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import NotFound from './pages/NotFound';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import ForgotPassword from './pages/auth/ForgotPassword';
+import ResetPassword from './pages/auth/ResetPassword';
 import VerifyEmail from './pages/auth/VerifyEmail';
 import VerifyEmailSent from './pages/auth/VerifyEmailSent';
 import ProtectedRoute from './components/layout/ProtectedRoute';
@@ -148,6 +149,7 @@ function AppShell({ splash }) {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/verify-email" element={<VerifyEmail />} />
           <Route path="/verify-email-sent" element={<VerifyEmailSent />} />
           {/* Client Dashboard */}
